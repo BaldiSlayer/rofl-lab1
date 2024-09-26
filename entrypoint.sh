072756592aa9d43c645b5530845def7df8e6ef5c
BaldiSlayer <lisov.a2005@yandex.ru>
2024-09-27 00:41:47 +0300

    added docker compose

---
+++ entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Start Ollama in the background.
+/bin/ollama serve &
+# Record Process ID.
+pid=$!
+
+# Pause for Ollama to start.
+sleep 5
+
+echo "🔴 Retrieve LLAMA3 model..."
+ollama pull llama3
+echo "🟢 Done!"
+
+# Wait for Ollama process to finish.
+wait $pid
+
