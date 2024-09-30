5e6a1311c90c3832e2113123e8072a97e4d70413
Alexey Lisov <lisov.a2005@yandex.ru>
2024-09-30 23:16:11 +0300

    test

--- internal/app/backend/mclient/mockclient.go
+++ internal/app/backend/mclient/mockclient.go
@@ -1,11 +1,13 @@
 package mclient
 
+import "github.com/BaldiSlayer/rofl-lab1/internal/app/backend/models"
+
 type Mock struct{}
 
 func (mc *Mock) Ask(question string) (string, error) {
 	return question, nil
 }
 
-func (mc *Mock) AskWithContext(question string, answerContext []string) (string, error) {
+func (mc *Mock) AskWithContext(question string, answerContext []models.QAPair) (string, error) {
 	return question, nil
 }
--- internal/app/backend/vdatabase/mock.go
+++ internal/app/backend/vdatabase/mock.go
@@ -1,5 +1,7 @@
 package vdatabase
 
+import "github.com/BaldiSlayer/rofl-lab1/internal/app/backend/models"
+
 // CMem - реализация векторной базы данных с помощью chromem-go
 type Mock struct {
 }
@@ -8,8 +10,11 @@ func (cm *Mock) Init(baseURL string, contextFile string) error {
 	return nil
 }
 
-func (cm *Mock) GetSimilar(question string) ([]string, error) {
-	return []string{
-		question,
+func (cm *Mock) GetSimilar(question string) ([]models.QAPair, error) {
+	return []models.QAPair{
+		{
+			Question: question,
+			Answer:   "idk",
+		},
 	}, nil
 }
