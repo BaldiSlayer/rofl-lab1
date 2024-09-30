948c459eb64efab940502544da6e8d24813bf3df
HumsterProgrammer <denis_ka_2307@mail.ru>
2024-09-30 17:27:01 +0300

    fix

--- parser/trsrules/parseTRS_test.go
+++ parser/trsrules/parseTRS_test.go
@@ -7,7 +7,7 @@ import (
 func TestLexerWithPeano(t *testing.T){
 	text := "variables = x,y,z\n f(x,S(y)) = S(f(x,y)) \n\r f(x, T) = T-------------S(x) = x+1\nf(x,y)=    x+2*y"
 	//Peano grammar
-	p := Parser{text, [], 0}
+	p := Parser{text : text}
 	err := p.Lexer()
 	
 	if err != nil {
