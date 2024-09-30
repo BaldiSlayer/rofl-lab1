e2ff793b3aae30d941e22d4a73864eefdebdf899
HumsterProgrammer <denis_ka_2307@mail.ru>
2024-09-30 18:12:47 +0300

    add another test for trsparser

--- parser/trsrules/parseTRS_test.go
+++ parser/trsrules/parseTRS_test.go
@@ -4,13 +4,31 @@ import (
 	"testing"
 )
 
+const (
+	peano string = "variables = x,y,z\n f(x,S(y)) = S(f(x,y)) \n\r f(x, T) = T-------------S(x) = x+1\nf(x,y)=    x+2*y"
+)
+
 func TestLexerWithPeano(t *testing.T) {
-	text := "variables = x,y,z\n f(x,S(y)) = S(f(x,y)) \n\r f(x, T) = T-------------S(x) = x+1\nf(x,y)=    x+2*y"
 	//Peano grammar
-	p := Parser{text: text}
+	p := Parser{text: peano}
 	err := p.Lexer()
 
 	if err != nil {
 		t.Errorf("Should not produce an error")
+	}
+}
+
+func TestParserWithPeano(t *testing.T){
+	p := Parser{text: peano}
+	err := p.Lexer()
+	if err != nil{
+		t.Error(err)
+	}
+	err = p.parseTRS()
+	if err != nil{
+		t.Error(err)
+	}
+	if p.lexem[p.index].lexType != lexSEPARATOR{
+		t.Errorf("Expected separator, but find %d lexem", p.lexem[p.index].lexType)
 	}
 }
