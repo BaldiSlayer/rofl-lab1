31725520a6dbf2ba7a28c8b3f8ad5c3b132660ff
Alexandr Starovoytov <aistarovoytov@yandex.ru>
2024-10-03 21:35:08 +0300

    Add errors handling

--- pkg/trsparser/errors.go
+++ pkg/trsparser/errors.go
@@ -1,10 +1,25 @@
 package trsparser
 
+import (
+	"errors"
+
+	"github.com/BaldiSlayer/rofl-lab1/internal/parser/trsinterprets"
+)
+
 func (e *ParseError) Error() string {
 	return e.Summary
 }
 
 func toParseError(err error) error {
-	// TODO: convert
-	return err
+	var parseError *trsinterprets.ParseError
+	if errors.As(err, &parseError) {
+		return &ParseError{
+			LlmMessage: parseError.LlmMessage(),
+			Summary:    parseError.Error(),
+		}
+	}
+	return &ParseError{
+		LlmMessage: err.Error(),
+		Summary:    err.Error(),
+	}
 }
