1593ad19d4ef3ae19697b3a35017697754c79c87
Alexandr Starovoytov <aistarovoytov@yandex.ru>
2024-10-04 15:34:36 +0300

    Add parser cmd

---
+++ cmd/parser/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"io"
+	"log/slog"
+	"os"
+
+	"github.com/BaldiSlayer/rofl-lab1/pkg/trsparser"
+)
+
+func main() {
+	r := bufio.NewReader(os.Stdin)
+	input, err := io.ReadAll(r)
+	if err != nil {
+		slog.Error("error while reading input string", "err", err)
+		os.Exit(1)
+	}
+
+	trs, err := trsparser.Parser{}.Parse(string(input))
+	if err != nil {
+		slog.Error("error while parsing input string", "err", err)
+		os.Exit(1)
+	}
+
+	w := bufio.NewWriter(os.Stdout)
+	defer w.Flush()
+
+	encoder := json.NewEncoder(w)
+	encoder.SetIndent("", "  ")
+	err = encoder.Encode(trs)
+	if err != nil {
+		slog.Error("error encoding trs", "err", err)
+	}
+}
