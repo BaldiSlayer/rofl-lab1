8d6911c91a6945abf8f11dca89242b183d903541
Alexey Lisov <77489413+BaldiSlayer@users.noreply.github.com>
2024-10-10 12:16:23 +0300

    remove useless (#40)
    * t
    
    * test

--- internal/app/tgbot/ustorage/ustorage.go
+++
@@ -1,8 +0,0 @@
-package ustorage
-
-import "github.com/BaldiSlayer/rofl-lab1/internal/app/tgbot/models"
-
-type UserDataStorage interface {
-	SetState(userID int64, state models.UserState) error
-	GetState(userID int64) models.UserState
-}
