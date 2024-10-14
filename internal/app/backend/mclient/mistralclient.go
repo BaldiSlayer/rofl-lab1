6c3c37f0ac9693fc01867db22595704b1c181f95
Alexandr Starovoytov <aistarovoytov@yandex.ru>
2024-10-14 13:57:08 +0300

    Codegen llm client

---
+++ internal/app/backend/mclient/mistralclient.go
@@ -0,0 +1,17 @@
+package mclient
+
+import "github.com/BaldiSlayer/rofl-lab1/internal/app/backend/models"
+
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=config.yaml ../../../../docs/trs-parser-api.yaml
+
+var _ ModelClient = (*Mistral)(nil)
+
+type Mistral struct{}
+
+func (mc *Mistral) Ask(question string) (string, error) {
+	return question, nil
+}
+
+func (mc *Mistral) AskWithContext(question string, answerContext []models.QAPair) (string, error) {
+	return question, nil
+}
