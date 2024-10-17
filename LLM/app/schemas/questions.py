e6334174c2cfd1fcd1f8ea36817626de19d76300
Alexandr Starovoytov <aistarovoytov@yandex.ru>
2024-10-17 20:23:57 +0300

    Fix

--- LLM/app/schemas/questions.py
+++ LLM/app/schemas/questions.py
@@ -1,9 +1,16 @@
 from typing import List, Optional
 from pydantic import BaseModel
 
+
+# Модель для вопроса и ответа
+class QuestionAnswer(BaseModel):
+    question: str
+    answer: str
+
+
 # Модель запроса для process_questions
 class ProcessQuestionsRequest(BaseModel):
-    questions_list: List[str]
+    questions_list: List[QuestionAnswer]
     use_saved: Optional[bool] = False
     filename: Optional[str] = "vectorized_data"
 
@@ -13,12 +20,6 @@ class GetChatResponseRequest(BaseModel):
     prompt: str
     context: Optional[str] = None
     model: Optional[str] = "open-mistral-7b"
-
-
-# Модель для вопроса и ответа
-class QuestionAnswer(BaseModel):
-    question: str
-    answer: str
 
 
 # Модель запроса для добавления новых вопросов
