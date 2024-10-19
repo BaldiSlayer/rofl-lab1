b0e8e678efc557d25dd189be2d9c82c71f7a31f8
Alexandr Starovoytov <aistarovoytov@yandex.ru>
2024-10-19 17:02:33 +0300

    Add TODOs

--- internal/app/usecases/trs.go
+++ internal/app/usecases/trs.go
@@ -3,13 +3,26 @@ package usecases
 import "github.com/BaldiSlayer/rofl-lab1/pkg/trsparser"
 
 func ExtractFormalTrs(request string) (trsparser.Trs, string, error) {
+
+	// TODO: сходить в клиент formalize
+
+	// TODO: сходить в парсер
+
 	return trsparser.Trs{}, "", nil
 }
 
 func FixFormalTrs(request, formalTrs string, parseError trsparser.ParseError) (trsparser.Trs, string, error) {
+
+	// TODO: сходить в клиент formalize
+
+	// TODO: сходить в парсер
+
 	return trsparser.Trs{}, "", nil
 }
 
 func InterpretFormalTrs(trsparser.Trs) (string, error) {
+
+	// TODO: сходить в клиент interpret
+
 	return "", nil
 }
