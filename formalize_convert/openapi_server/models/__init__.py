46106da02be79e36d228de8daad90f543ffae2d5
Alexandr Starovoytov <aistarovoytov@yandex.ru>
2024-10-20 13:50:52 +0300

    Codegen formalize server

---
+++ formalize_convert/openapi_server/models/__init__.py
@@ -0,0 +1,4 @@
+# flake8: noqa
+# import models into model package
+from openapi_server.models.formalize_request import FormalizeRequest
+from openapi_server.models.formalize_result import FormalizeResult
