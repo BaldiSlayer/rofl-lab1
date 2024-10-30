1a05354512dc2f78e28e6dc4662266b450977b5c
Alexandr Starovoytov <aistarovoytov@yandex.ru>
2024-10-30 19:55:41 +0300

    Fix

--- internal/app/tgbot/controllers/kbase.go
+++ internal/app/tgbot/controllers/kbase.go
@@ -1,10 +1,8 @@
 package controllers
 
 import (
-	"context"
+	"errors"
 	"fmt"
-	"log/slog"
-	"time"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 
@@ -12,56 +10,23 @@ import (
 	"github.com/BaldiSlayer/rofl-lab1/internal/app/usecases"
 )
 
-const (
-	waitForKBQuestionTimeout = 10 * time.Second
-)
-
 func (controller *Controller) handleKnowledgeBaseRequest(update tgbotapi.Update) (models.UserState, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), waitForKBQuestionTimeout)
-	defer cancel()
-
-	// FIXME: антипаттерн так получать значения?
-	var answer string
-	var err error
-	doneChan := make(chan struct{}, 1)
-	go func() {
-		answer, err = usecases.AskKnowledgeBase(controller.ModelClient, update.Message.Text)
-		if err != nil {
-			slog.Error(err.Error())
-		}
-		doneChan <- struct{}{}
-	}()
-
-	select {
-	case <-ctx.Done():
-		err = controller.Bot.SendMessage(
+	answer, err := usecases.AskKnowledgeBase(controller.ModelClient, update.Message.Text)
+	if err != nil {
+		err = errors.Join(err, controller.Bot.SendMessage(
 			update.Message.Chat.ID,
-			"Таймаут при запросе к Базе Знаний, введите новый запрос",
-		)
-		if err != nil {
-			return models.WaitForRequest, err
-		}
-	case <-doneChan:
-		if err != nil {
-			err = controller.Bot.SendMessage(
-				update.Message.Chat.ID,
-				"Ошибка при запросе к Базе Знаний, введите новый запрос",
-			)
-			if err != nil {
-				return models.WaitForRequest, err
-			}
-
-			return models.WaitForRequest, err
-		}
-
-		err = controller.Bot.SendMessage(
-			update.Message.Chat.ID,
-			fmt.Sprintf("Ответ Базы Знаний: %s", answer),
-		)
-		if err != nil {
-			return models.WaitForRequest, err
-		}
+			"Ошибка при запросе к Базе Знаний, введите новый запрос",
+		))
+		return models.WaitForRequest, err
 	}
 
-	panic("unreachable")
+	err = controller.Bot.SendMessage(
+		update.Message.Chat.ID,
+		fmt.Sprintf("Ответ Базы Знаний: %s", answer),
+	)
+	if err != nil {
+		return models.WaitForRequest, err
+	}
+
+	return models.WaitForRequest, nil
 }
