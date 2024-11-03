0f80e0b34a8047f90d5b040beb9b806ad844cbbb
Alexandr Starovoytov <aistarovoytov@yandex.ru>
2024-11-03 12:30:05 +0300

    Add migration and healthchecks

---
+++ postgresql/final_schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE tfllab1.user_state (
+       user_id BIGINT PRIMARY KEY,
+       state INT NOT NULL
+);
+
+CREATE TABLE tfllab1.extraction_result (
+       user_id BIGINT PRIMARY KEY,
+       user_request TEXT,
+       formalize_result TEXT,
+       parse_result JSONB,
+       parse_error TEXT
+);
