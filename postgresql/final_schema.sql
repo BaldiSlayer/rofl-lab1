ccd0db54d76d6f445dcb3642dd079d90f024e2b3
Alexandr Starovoytov <aistarovoytov@yandex.ru>
2024-11-03 17:14:56 +0300

    Add migrate cmd example

--- postgresql/final_schema.sql
+++ postgresql/final_schema.sql
@@ -1,3 +1,5 @@
+-- export $(cat .env | xargs) && docker run -it --rm --network rofl-lab1_default -v $(pwd)/postgresql/migrations/:/migrations/migrations urbica/pgmigrate -d /migrations -t latest migrate -t 1 -c "port=5432 host=postgres dbname=$POSTGRES_DB user=$POSTGRES_USER password=$POSTGRES_PASSWORD"
+
 CREATE TABLE tfllab1.user_state (
        user_id BIGINT PRIMARY KEY,
        state INT NOT NULL
