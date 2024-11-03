a9fd314ae102a8d91cbf7529791252bb5e70e78a
Alexandr Starovoytov <aistarovoytov@yandex.ru>
2024-11-03 11:25:41 +0300

    Typo

--- internal/app/tgbot/ustorage/ustorage.go
+++ internal/app/tgbot/ustorage/ustorage.go
@@ -6,11 +6,11 @@ import (
 )
 
 type UserDataStorage interface {
-	SetState(userID int64, state models.UserState) error // +
+	SetState(userID int64, state models.UserState) error
 	SetTRS(userID int64, trs trsparser.Trs) error
 	SetFormalTRS(userID int64, formalTrs string) error
-	SetRequest(userID int64, request string) error       // +
-	SetParseError(userID int64, parseError string) error // +
+	SetRequest(userID int64, request string) error
+	SetParseError(userID int64, parseError string) error
 	GetState(userID int64) (models.UserState, error)
 	GetTRS(userID int64) (trsparser.Trs, error)
 	GetFormalTRS(userID int64) (string, error)
