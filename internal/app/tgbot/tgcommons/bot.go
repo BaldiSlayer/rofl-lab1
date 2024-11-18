f37dee21e23a1a831cf5e1e4ef2af0c2c970ce11
Alexandr Starovoytov <aistarovoytov@yandex.ru>
2024-11-18 17:56:39 +0300

    Add logs

--- internal/app/tgbot/tgcommons/bot.go
+++ internal/app/tgbot/tgcommons/bot.go
@@ -1,6 +1,7 @@
 package tgcommons
 
 import (
+	"log/slog"
 	"net/http"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
@@ -25,6 +26,15 @@ func NewBot(token string) (*Bot, error) {
 	_, err = bot.Request(wh)
 	if err != nil {
 		return nil, err
+	}
+
+	info, err := bot.GetWebhookInfo()
+	if err != nil {
+		slog.Error("failed to get webhook info", "error", err)
+	}
+
+	if info.LastErrorDate != 0 {
+		slog.Error("telegram callback failed", "error", info.LastErrorMessage)
 	}
 
 	return &Bot{
