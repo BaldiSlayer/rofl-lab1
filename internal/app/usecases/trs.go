816070fc0826cc02539344c58057af00ee541c6d
Alexandr Starovoytov <aistarovoytov@yandex.ru>
2024-11-26 19:23:20 +0300

    Refactor

--- internal/app/usecases/trs.go
+++ internal/app/usecases/trs.go
@@ -44,108 +44,41 @@ type ExtractData struct {
 }
 
 func (uc *TrsUseCases) ExtractFormalTrs(ctx context.Context, request string) (ExtractData, error) {
-	formalizedTrs, err := uc.formalize.Formalize(ctx, request)
-	for i := 0; i < retryMax && formalizedTrs.ErrorDescription != nil; i++ {
-		slog.Info("got error from formalize", "error", *formalizedTrs.ErrorDescription)
-		formalizedTrs, err = uc.formalize.FixFormalized(ctx, request, formalizedTrs.FormalizedTrs, *formalizedTrs.ErrorDescription)
+	result, err := uc.formalize.Formalize(ctx, request)
+	return uc.fixFormalTrs(ctx, request, result, err)
+}
+
+func (uc *TrsUseCases) FixFormalTrs(ctx context.Context, request, formalTrs, errorDescription string) (ExtractData, error) {
+	result, err := uc.formalize.FixFormalized(ctx, request, formalTrs, errorDescription)
+	return uc.fixFormalTrs(ctx, request, result, err)
+}
+
+func (uc *TrsUseCases) fixFormalTrs(ctx context.Context, request string, result formalizeclient.FormalizeResultDTO, err error) (ExtractData, error) {
+	if result.ErrorDescription != nil {
+		return ExtractData{}, fmt.Errorf("error formalizing trs: %s", *result.ErrorDescription)
+	}
+	for i := 0; i < retryMax && result.ErrorDescription != nil; i++ {
+		slog.Info("got error from formalize", "error", err)
+		result, err = uc.formalize.FixFormalized(ctx, request, result.FormalizedTrs, *result.ErrorDescription)
 	}
 	if err != nil {
 		return ExtractData{}, err
 	}
-	if formalizedTrs.ErrorDescription != nil {
-		return ExtractData{}, fmt.Errorf("error formalizing trs: %s", *formalizedTrs.ErrorDescription)
-	}
-
-	trs, err := uc.parser.Parse(formalizedTrs)
-	if err != nil {
-		return ExtractData{
-			Trs:           trsparser.Trs{},
-			FormalizedTrs: formalizedTrs,
-		}, err
-	}
-
-	return ExtractData{
-		Trs:           *trs,
-		FormalizedTrs: formalizedTrs,
-	}, nil
-}
-
-// TODO: refactor duplicate code in ExtractFormalTrs and FixFormalTrs
-func (uc *TrsUseCases) ExtractFormalTrs(ctx context.Context, request string) (ExtractData, error) {
-	formalizedTrs, err := uc.formalize.Formalize(ctx, request)
-	for i := 0; i < retryMax && result.ErrorDescription != nil; i++ {
-		slog.Info("got error from formalize", "error", *result.ErrorDescription)
-		result, err = uc.formalize.FixFormalized(ctx, request, result.FormalizedTrs, *result.ErrorDescription)
-	}
-	if err != nil {
-		return trsparser.Trs{}, "", err
-	}
 	if result.ErrorDescription != nil {
-		return trsparser.Trs{}, "", fmt.Errorf("error formalizing trs: %s", *result.ErrorDescription)
+		return ExtractData{}, fmt.Errorf("error formalizing trs: %s", *result.ErrorDescription)
 	}
 
 	trs, err := uc.parser.Parse(result.FormalizedTrs)
 	if err != nil {
 		return ExtractData{
 			Trs:           trsparser.Trs{},
-			FormalizedTrs: formalizedTrs,
+			FormalizedTrs: result.FormalizedTrs,
 		}, err
 	}
 
 	return ExtractData{
 		Trs:           *trs,
-		FormalizedTrs: formalizedTrs,
-	}, nil
-}
-
-func (uc *TrsUseCases) FixFormalTrs(ctx context.Context, request, formalTrs, errorDescription string) (ExtractData, error) {
-	formalizedTrs, err := uc.formalize.FixFormalized(ctx, request, formalTrs, errorDescription)
-	if err != nil {
-		return ExtractData{}, err
-	}
-
-	trs, err := uc.parser.Parse(formalizedTrs)
-	if err != nil {
-		return ExtractData{
-			Trs:           trsparser.Trs{},
-			FormalizedTrs: formalizedTrs,
-		}, err
-	}
-
-	return ExtractData{
-		Trs:           *trs,
-		FormalizedTrs: formalizedTrs,
-	}, nil
-}
-
-
-func (uc *TrsUseCases) FixFormalTrs(ctx context.Context, request, formalTrs, errorDescription string) (ExtractData, error) {
-	formalizedTrs, err := uc.formalize.FixFormalized(ctx, request, formalTrs, errorDescription)
-	if formalizedTrs.ErrorDescription != nil {
-		return ExtractData{}, fmt.Errorf("error formalizing trs: %s", *formalizedTrs.ErrorDescription)
-	}
-	for i := 0; i < retryMax && formalizedTrs.ErrorDescription != nil; i++ {
-		slog.Info("got error from formalize", "error", err)
-		formalizedTrs, err = uc.formalize.FixFormalized(ctx, request, formalizedTrs.FormalizedTrs, *formalizedTrs.ErrorDescription)
-	}
-	if err != nil {
-		return ExtractData{}, err
-	}
-	if formalizedTrs.ErrorDescription != nil {
-		return ExtractData{}, fmt.Errorf("error formalizing trs: %s", *formalizedTrs.ErrorDescription)
-	}
-
-	trs, err := uc.parser.Parse(formalizedTrs)
-	if err != nil {
-		return ExtractData{
-			Trs:           trsparser.Trs{},
-			FormalizedTrs: formalizedTrs,
-		}, err
-	}
-
-	return ExtractData{
-		Trs:           *trs,
-		FormalizedTrs: formalizedTrs,
+		FormalizedTrs: result.FormalizedTrs,
 	}, nil
 }
 
