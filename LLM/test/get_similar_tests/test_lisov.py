ab2b887e4fb4ce9d3fd54332d00b3e2e15844987
Alexey Lisov <lisov.a2005@yandex.ru>
2024-12-07 01:56:29 +0300

    fix

--- LLM/test/get_similar_tests/test_lisov.py
+++ LLM/test/get_similar_tests/test_lisov.py
@@ -46,3 +46,101 @@ def test_nearest_nka_5():
     should_include_checker(question, should_include)
 
 
+def test_epsilon_closure_1():
+    question = "Опиши алгоритм нахождения эпсилон замыкания для каждой из вершин автомата"
+    should_include = [
+        "Опиши алгоритм нахождения эпсилон замыкания для каждой из вершин автомата?",
+    ]
+
+    should_include_checker(question, should_include)
+
+
+def test_epsilon_closure_2():
+    question = "Как мне найти эпсилон замыкание для каждой из вершин автомата?"
+    should_include = [
+        "Опиши алгоритм нахождения эпсилон замыкания для каждой из вершин автомата?",
+    ]
+
+    should_include_checker(question, should_include)
+
+
+def test_epsilon_closure_3():
+    question = "Нахождение эпсилон замыкания для все вершин автомата?"
+    should_include = [
+        "Опиши алгоритм нахождения эпсилон замыкания для каждой из вершин автомата?",
+    ]
+
+    should_include_checker(question, should_include)
+
+
+def test_prefix_languages_1():
+    question = "Какой язык называется префиксным (беспрефиксным)"
+    should_include = [
+        "Какой язык называется префиксным (беспрефиксным)",
+    ]
+
+    should_include_checker(question, should_include)
+
+
+def test_prefix_languages_2():
+    question = "Дай определение префиксного языка"
+    should_include = [
+        "Какой язык называется префиксным (беспрефиксным)",
+    ]
+
+    should_include_checker(question, should_include)
+
+
+def test_prefix_languages_3():
+    question = "Дай определение беспрефиксного языка"
+    should_include = [
+        "Какой язык называется префиксным (беспрефиксным)",
+    ]
+
+    should_include_checker(question, should_include)
+
+
+def test_prefix_languages_4():
+    question = "Префиксный язык определение"
+    should_include = [
+        "Какой язык называется префиксным (беспрефиксным)",
+    ]
+
+    should_include_checker(question, should_include)
+
+
+def test_prefix_languages_5():
+    question = "Беспрефиксный язык определение"
+    should_include = [
+        "Какой язык называется префиксным (беспрефиксным)",
+    ]
+
+    should_include_checker(question, should_include)
+
+
+def test_count_n_len_words_1():
+    question = "Опиши алгоритм подсчета количества слов определенной длины в заданном регулярном языке"
+    should_include = [
+        "Опиши алгоритм подсчета количества слов определенной длины в заданном регулярном языке",
+    ]
+
+    should_include_checker(question, should_include)
+
+
+def test_count_n_len_words_2():
+    question = "Алгоритм подсчета количества слов определенной длины в заданном регулярном языке"
+    should_include = [
+        "Опиши алгоритм подсчета количества слов определенной длины в заданном регулярном языке",
+    ]
+
+    should_include_checker(question, should_include)
+
+
+def test_count_n_len_words_3():
+    question = "Как подсчитать количество слов определенной длины в заданном регулярном языке"
+    should_include = [
+        "Опиши алгоритм подсчета количества слов определенной длины в заданном регулярном языке",
+    ]
+
+    should_include_checker(question, should_include)
+
